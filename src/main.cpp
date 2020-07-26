bb4b35edf03128bbed2a29a8c231fcf502f372ac
ilj900 <ilj900@gmail.com>
2020-07-26 17:41:29 +0300

    Added stb_image_write.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-#include "vector3.h"
+#include "stb_image_write.h"
 
-#include <iostream>
-
-int main() {
-    Vector3 vec(1, 2, 3);
-    std::cout<< vec.X << " " << vec.G << " " << vec.Data[2] << std::endl;
+#include <vector>
+#include <cstdint>
+int main()
+{
+    std::uint32_t Width = 256;
+    std::uint32_t Height = 256;
+    std::uint32_t NumberOfChannels = 4;
+    std::vector<std::uint8_t> Image(Width * Height * NumberOfChannels);
+    for (int i = 0; i < Height; ++i)
+    {
+        for (int j = 0; j < Width; ++j)
+        {
+            Image[(i * Width + j) * NumberOfChannels] = 255;
+            Image[(i * Width + j) * NumberOfChannels + 1] = 0;
+            Image[(i * Width + j) * NumberOfChannels + 2] = 255;
+            Image[(i * Width + j) * NumberOfChannels + 3] = 255;
+        }
+    }
+    stbi_write_png("Test.png", Width, Height, NumberOfChannels, Image.data(), 0);
     return 0;
 }
